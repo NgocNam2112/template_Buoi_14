2c343d5dd0d32fbce0e7bde2c41f3d921ff7924c
NgocNam2112 <namnguyen2k2112@gmail.com>
2022-11-27 12:33:50 +0700

    simple crud app

--- index.mjs
+++ index.mjs
@@ -4,46 +4,139 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 var wrapList = document.getElementById("wrap_list");
+const formSubmit = document.getElementById("formSubmit");
+const submitFormBtn = document.getElementById("submitForm");
+
+const API = document.getElementById("API");
+const Cors = document.getElementById("Cors");
+const Category = document.getElementById("Category");
+const HTTPS = document.getElementById("HTTPS");
+const Image = document.getElementById("img");
+const Description = document.getElementById("description");
+const Link = document.getElementById("link");
+
+let entryItem;
 
 btn.onclick = function () {
   modal.style.display = "block";
+  submitFormBtn.value = "Submit";
 };
 
 span.onclick = function () {
   modal.style.display = "none";
+  entryItem = null;
+  resetInputForm();
 };
 
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    entryItem = null;
+    resetInputForm();
   }
 };
 
 // axios
 
+const validateForm = () => {
+  if (
+    !API.value ||
+    !Cors.value ||
+    !Category.value ||
+    !Image.value ||
+    !Description.value ||
+    !Link.value
+  ) {
+    return true;
+  }
+};
+
+const resetInputForm = () => {
+  API.value = "";
+  Cors.value = "";
+  Category.value = "";
+  HTTPS.value = "yes";
+  Image.value = "";
+  Description.value = "";
+  Link.value = "";
+};
+
+const handleCreateNewEntry = async () => {
+  document.getElementById("common__error").style.display = "none";
+  if (validateForm()) {
+    document.getElementById("common__error").style.display = "block";
+    return;
+  }
+  const formData = {
+    API: API.value,
+    Description: Description.value,
+    image: Image.value,
+    Auth: "",
+    HTTPS: HTTPS.value === "yes" ? true : false,
+    Cors: Cors.value,
+    Link: Link.value,
+    Category: Category.value,
+  };
+  const data = await axios.post(`${BASE_URL}/get-list/entries`, {
+    ...formData,
+  });
+  if (data.status === 201) {
+    modal.style.display = "none";
+    resetInputForm();
+    fetchListEntries();
+  }
+};
+
+const handleUpdateEntry = async () => {
+  document.getElementById("common__error").style.display = "none";
+  if (validateForm()) {
+    document.getElementById("common__error").style.display = "block";
+    return;
+  }
+  const formData = {
+    API: API.value,
+    Description: Description.value,
+    image: Image.value,
+    Auth: "",
+    HTTPS: HTTPS.value === "yes" ? true : false,
+    Cors: Cors.value,
+    Link: Link.value,
+    Category: Category.value,
+  };
+  const data = await axios.put(`${BASE_URL}/get-list/entries/${entryItem.id}`, {
+    ...formData,
+  });
+  if (data.status === 200) {
+    modal.style.display = "none";
+    resetInputForm();
+    fetchListEntries();
+  }
+};
+
+formSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (submitFormBtn.value === "Update") {
+    handleUpdateEntry();
+  } else {
+    handleCreateNewEntry();
+  }
+});
+
+const handleDeleteEntry = async (item) => {
+  try {
+    const data = await axios.delete(`${BASE_URL}/get-list/entries/${item.id}`);
+    if (data.status === 200) {
+      fetchListEntries();
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
 const fetchListEntries = async () => {
+  wrapList.innerHTML = "";
   try {
     const { data } = await axios.get(`${BASE_URL}/get-list/entries`);
-    console.log("data", data);
-    //     const layout = data
-    //       .map(
-    //         (item) => `<div class="card" key=${item.id}>
-    //     <img onclick="handleDeleteEntry()" src="https://static.thenounproject.com/png/2854151-200.png" alt="" class="dots">
-    //     <img src="${item.image}" alt="Avatar" class="avatar">
-    //     <div class="container">
-    //         <h4><b>${item.Description}</b></h4>
-    //         <p>${item.Category}</p>
-    //     </div>
-    // </div>`
-    //       )
-    //       .join("");
-
-    const handleDeleteEntry = async () => {
-      console.log("click here");
-      // await axios.delete(`${BASE_URL}/get-list/entries`, {
-      //   id: "6",
-      // });
-    };
 
     data.forEach((item) => {
       const description = document.createElement("b");
@@ -72,13 +165,31 @@ const fetchListEntries = async () => {
       );
       imageDot.setAttribute("alt", "");
       imageDot.setAttribute("class", "dots");
-      imageDot.onclick = handleDeleteEntry;
+      imageDot.onclick = (event) => {
+        event.stopPropagation();
+        handleDeleteEntry(item);
+      };
 
       const card = document.createElement("div");
       card.setAttribute("class", "card");
       card.appendChild(imageDot);
       card.appendChild(avatar);
       card.appendChild(wrapEntryInfo);
+
+      card.onclick = () => {
+        entryItem = {
+          ...item,
+        };
+        modal.style.display = "block";
+        API.value = item.API;
+        Cors.value = item.Cors;
+        Category.value = item.Category;
+        HTTPS.value = item.HTTPS ? "yes" : "no";
+        Image.value = item.image;
+        Description.value = item.Description;
+        Link.value = item.Link;
+        submitFormBtn.value = "Update";
+      };
 
       wrapList.appendChild(card);
     });
--- index2.js
+++
@@ -1,5 +0,0 @@
-const handleDeleteEntry = async () => {
-  await axios.delete(`${BASE_URL}/get-list/entries`, {
-    id: "6",
-  });
-};
